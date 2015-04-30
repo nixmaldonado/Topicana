c90b51779d1b991a7463d746719d6215c4d30527
Nicolas Maldonado <nixmaldonado@gmail.com>
2015-04-30 16:36:27 -0300

    refactoring the tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe User, type: :model do
 
   describe '#fav_topic!' do
     before do
-      @user = FactoryGirl.create(:user)
+      @user  = FactoryGirl.create(:user)
       @topic = FactoryGirl.create(:topic)
     end
 
@@ -27,6 +27,7 @@ RSpec.describe User, type: :model do
       before do
         @user.fav_topic!(@topic)
       end
+
       it 'favorites a topic' do
         result = @user.favorited_topics.find_by_id(@topic)
         expect(result).to eq(@topic)
@@ -37,29 +38,32 @@ RSpec.describe User, type: :model do
       before do
         @user.fav_topic!(@topic)
       end
+
       it 'returns nil' do
         result =  @user.fav_topic!(@topic)
         expect(result).to eq(nil)
       end
     end
-
   end
 
   describe '#unfav_topic!' do
     before do
-        @user  = FactoryGirl.create(:user)
-        @topic = FactoryGirl.create(:topic)
+      @user  = FactoryGirl.create(:user)
+      @topic = FactoryGirl.create(:topic)
     end
+
     context 'when a topic is already favorite' do
       before do
         @user.fav_topic!(@topic)
       end
+
       it 'unfavorites a topic' do
         @user.unfav_topic!(@topic)
         result = @user.favorited_topics.include?(@topic)
         expect(result).to eq(false)
       end
     end
+
     context 'when a topic is not favorited' do
       it 'returns nil' do
         result = @user.unfav_topic!(@topic)
@@ -70,11 +74,12 @@ RSpec.describe User, type: :model do
 
   describe '#already_favorited?' do
     before do
-      @user = FactoryGirl.create(:user)
+      @user  = FactoryGirl.create(:user)
       @topic = FactoryGirl.create(:topic)
-      @user.favorited_topics << @topic
     end
+
     it 'returns topic if topic already favorited' do
+      @user.favorited_topics << @topic
       result = @user.already_favorited?(@topic)
       expect(result).to eq(@topic)
     end
@@ -84,10 +89,10 @@ RSpec.describe User, type: :model do
 
   describe '#flag_comment!' do
     before do
-      @user = FactoryGirl.create(:user)
-      @topic = FactoryGirl.create(:topic)
+      @user    = FactoryGirl.create(:user)
       @comment = FactoryGirl.create(:comment)
     end
+
     context 'when a comment is not flagged' do
       it 'flags a comment' do
         @user.flag_comment!(@comment)
@@ -95,11 +100,10 @@ RSpec.describe User, type: :model do
         expect(result).to eq(true)
       end
     end
+
     context 'when a comment is already flagged' do
-      before do
-        @user.flagged_comments << @comment
-      end
       it 'returns nil' do
+        @user.flagged_comments << @comment
         result = @user.flag_comment!(@comment)
         expect(result).to eq(nil)
       end
@@ -108,20 +112,22 @@ RSpec.describe User, type: :model do
 
   describe '#unflag_comment!' do
     before do
-      @user = FactoryGirl.create(:user)
-      @topic = FactoryGirl.create(:topic)
+      @user    = FactoryGirl.create(:user)
       @comment = FactoryGirl.create(:comment)
     end
+
     context 'when a comment is already flagged'do
       before do
         @user.flagged_comments << @comment
       end
+
       it 'unflags the comment' do
         @user.unflag_comment!(@comment)
         result = @user.flagged_comments.include?(@comment)
         expect(result).to eq(false)
       end
     end
+
     context 'when a comment is not flagged' do
       it 'returns nil' do
         result = @user.unflag_comment!(@comment)
@@ -132,20 +138,22 @@ RSpec.describe User, type: :model do
 
   describe '#aleady_flagged?' do
     before do
-      @user = FactoryGirl.create(:user)
-      @topic = FactoryGirl.create(:topic)
+      @user    = FactoryGirl.create(:user)
       @comment = FactoryGirl.create(:comment)
     end
+
     context 'when the comment is not flagged' do
       it 'returns nil' do
         result = @user.already_flagged?(@comment)
         expect(result).to eq(nil)
       end
     end
+
     context 'when the comment is flagged' do
       before do
         @user.flagged_comments << @comment
       end
+
       it 'returns the comment' do
         result = @user.already_flagged?(@comment)
         expect(result).to eq(@comment)
